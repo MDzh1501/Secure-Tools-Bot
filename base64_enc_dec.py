ddb7c0d50f839171a1142095c184423fc5ba029c
MDzh1501 <coolmarik1501@gmail.com>
2025-06-12 21:19:22 +0300

    Decoding func for bot

--- base64_enc_dec.py
+++ base64_enc_dec.py
@@ -1,12 +1,30 @@
 import base64
 import binascii
 
-def base64_encode(text: str, format: str) -> str:
+
+def base64_encode(text: str, encoding: str) -> str:
     try:
-        data = text.encode(format.lower())
+        data = text.encode(encoding.lower())
         encoded = base64.b64encode(data)
 
-        return encoded.decode(format.lower())
+        return encoded.decode(encoding.lower())
    
     except (UnicodeEncodeError, LookupError):
-        return f"❌ Encoding failed. Unsupported format: {format}"
+        return f"❌ Encoding failed. Unsupported format: {encoding}"
+
+
+def base64_decode(text: str, encoding: str) -> str:
+    try:
+        data = text.encode(encoding.lower())
+        decoded = base64.b64decode(data)
+
+        return decoded.decode(encoding.lower())
+    
+    except (binascii.Error, ValueError):
+        return "❌ Base64 decoding failed. Input is not valid base64."
+    
+    except UnicodeDecodeError:
+        return f"❌ Failed to decode bytes to text using '{encoding.lower()}'."
+    
+    except LookupError:
+        return f"❌ Unsupported encoding: {encoding}"
